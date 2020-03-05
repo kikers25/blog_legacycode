21fe586e8636187b145ca88719e00d628e423570
Enrique Martín Martín <kikers25@gmail.com>
2020-03-05 20:12:14 +0100

    Created controller that uses the user service

---
+++ src/main/java/com/blog/web/Controller.java
@@ -0,0 +1,25 @@
+package com.blog.web;
+
+import com.blog.domain.User;
+import com.blog.service.UserAdapter;
+import com.blog.service.UserService;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Controller  extends HttpServlet {
+
+
+    protected static final String USER = "USER";
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) {
+
+        UserService userService = new UserService(new UserAdapter());
+        User user = userService.getUser(request.getParameter(USER));
+
+        // Do something
+    }
+
+}
